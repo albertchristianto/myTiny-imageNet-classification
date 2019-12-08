8e9302d0a7ceb10057b2854bf55d84a1d2065b35
albertchristianto <32095448+albertchristianto@users.noreply.github.com>
2019-12-08 15:16:23 +0800

    Update myGetModel.py

--- model/myGetModel.py
+++ model/myGetModel.py
@@ -8,14 +8,14 @@ from myAlexnet import myAlexnet
 from myOwnModel import myOwnModel
 from myResNet18 import myResNet18
 
-def getModels(trunk='myVGG16', numClass = 0, pretrained_choice=False):
-	'''this program is used to build CNN model with input size 56x56x3'''
+def getModels(inp_size, trunk='myVGG16', numClass = 0, pretrained_choice=False):
+	'''this program is used to build CNN model with input size inp_sizexinp_sizex3'''
 	if trunk == 'myVGG16' and numClass!= 0:
-		myModel = myVGG16(numClass)
+		myModel = myVGG16(numClass,inp_size)
 
 		if pretrained_choice:
 			vgg16_url = 'https://download.pytorch.org/models/vgg16-397923af.pth'
-			model_path = 'pretrained_weight'
+			model_path = '../input/pretrained_resnet50'
 			load_until = 20
 			vgg_state_dict = model_zoo.load_url(vgg16_url, model_dir=model_path)
 
@@ -27,11 +27,11 @@ def getModels(trunk='myVGG16', numClass = 0, pretrained_choice=False):
 			state.update(weights_load)
 			myModel.load_state_dict(state)
 	elif trunk == 'myAlexnet' and numClass!= 0:
-		myModel = myAlexnet(numClass)
+		myModel = myAlexnet(numClass,inp_size)
 	elif trunk == 'mine' and numClass!= 0:
-		myModel = myOwnModel(numClass)
+		myModel = myOwnModel(numClass,inp_size)
 	elif trunk == 'myResNet18' and numClass!= 0:
-		myModel = myResNet18(pretrained=pretrained_choice, num_classes=numClass)
+		myModel = myResNet18(inp_size, pretrained=pretrained_choice, num_classes=numClass)
 	else:
 		print('Please define the model or number of class that you are using now')
 		exit()
